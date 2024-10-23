9edab77cc711daf942cb31628621ff32c27210a5
Poorva <poorva2374@gmail.com>
2024-10-23 15:18:57 +0530

    HBS and Partials implementation

--- server/server.js
+++ server/server.js
@@ -1,26 +1,56 @@
-const express = require("express")
-const connectDb = require("./config/dbConnection");
+const express= require("express");
+const connectDb= require("./config/dbConnection");
 const errorHandler = require("./middlewares/errorHandler");
-const cors = require("cors");
+const cors= require ("cors");
+const hbs = require("hbs");
+const path = require("path");
 
-//env file config
+// env file config
 const dotenv = require("dotenv");
 dotenv.config();
 
 connectDb();
 const app = express();
-const port = process.env.PORT || 5000;
+const port= process.env.PORT || 5000;
 
-
-app.use(cors());    
 app.use(express.json());
+app.use(cors());
 
+//Error handling
 app.use(errorHandler);
 
-app.get("/", (req, res) => {
-    res.send("working perfectly!")
-})
+// using hbs
+app.set('view engine','hbs');
+
+
+//Routes below
+app.get("/",(req,res)=>{
+    res.send("working")
+});
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+app.get("/home",(req,res)=>{
+    // let user =  user.findone({id:})
+    res.render("home",{ 
+        username:"Poorva",
+        age : 20,
+    })
+});
+
+
+app.get("/user",(req,res)=>{
+    // let user =  user.findone({id:})
+    const users = [
+        { username: "Poorva", age: 20 },
+        { username: "Ashish", age: 22 },
+        { username: "Divyam", age: 21 }
+    ];
+    
+    res.render("user",{users})
+});
+
 
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`)
-})
+    console.log(`Server is running on port http://localhost:${port}`);
+});
